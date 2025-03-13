1f17a9c9a50f6fd2b1e6814e0553f75c11fbf56b
Sean-mn <b01080080329@gmail.com>
2025-03-13 23:49:30 +0900

    2024/03/13 p.m 11:50

---
+++ 2025_1_2/Assets/Scripts/Entity/Player/PlayerAttack.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [Header("Stat")]
+    [SerializeField] private float _attackDamage;
+    public float AttackDamage => _attackDamage;
+
+    [SerializeField] private float _attackDelay;
+    private bool _isAttacking = false;
+
+    [Header("Reference")]
+    [SerializeField] private GameObject _arm;
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !_isAttacking)
+        {
+            StartCoroutine(SetAttackMove());
+        }
+    }
+
+    private IEnumerator SetAttackMove()
+    {
+        _isAttacking = true; // 공격 시작
+
+        yield return new WaitForSeconds(0.5f);
+
+        Quaternion startRot = _arm.transform.localRotation;
+        Quaternion attackRot1 = Quaternion.Euler(-20, 0, 0) * startRot;
+        Quaternion attackRot2 = Quaternion.Euler(30, 0, 0) * startRot;
+
+        yield return RotateArm(attackRot1, _attackDelay);
+        yield return RotateArm(attackRot2, _attackDelay);
+        yield return RotateArm(startRot, _attackDelay);
+
+        _isAttacking = false;
+    }
+
+    private IEnumerator RotateArm(Quaternion targetRot, float duration)
+    {
+        float time = 0;
+        Quaternion initRot = _arm.transform.localRotation;
+
+        while (time < duration)
+        {
+            _arm.transform.localRotation = Quaternion.Lerp(initRot, targetRot, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        _arm.transform.localRotation = targetRot;
+    }
+}
